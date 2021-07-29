251e5ba06be53ed4488f3543ca94c9c89a9411a8
Chetan-Gudagamanal <chetanbg95@gmail.com>
2021-07-29 12:43:40 +0530

    fixing bug

--- controllers/handleRegister.js
+++ controllers/handleRegister.js
@@ -14,7 +14,33 @@ export const handleRegister=async(req,res,db)=>{
         else{
             try{
                 await urlUsers.deleteOne(existingUser)
-                const urlUser=await saveUserRegistrationDetails(firstName,lastName,userEmail,password,res)
+                //const urlUser=await saveUserRegistrationDetails(firstName,lastName,userEmail,password,res)
+                const salt = await bcrypt.genSalt(10)
+                const passwordHash= await bcrypt.hash(password,salt)
+                
+                const urlUser=new urlUsers(
+                    {
+                        firstName,
+                        lastName,
+                        userEmail,
+                        password:passwordHash,
+                        active:false
+                    }
+                )
+                
+                await urlUser.save()
+
+                const secretKey=process.env.SECRET_KEY || "my_secret";
+                const key=secretKey+urlUser.password
+                const payload={
+                    email:urlUser.userEmail,
+                    id:urlUser._id
+                }
+                const token=jwt.sign(payload,key,{expiresIn:"10m"})
+                const verificationLink=`${process.env.CLIENT_BASE_URL}/verify_email/${urlUser._id}/${token}`
+                // console.log(urlUser)
+                console.log(verificationLink)
+                handleSendRegistrationEmail(urlUser.userEmail,verificationLink)
                 res.status(200).json(urlUser)
             }
             catch(err){
